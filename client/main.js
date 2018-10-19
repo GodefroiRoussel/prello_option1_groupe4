17a6247f67b77a6f683f9006f41a6ccee39464ec
Vincent Herreros <vincent-herreros@hotmail.fr>
2018-10-19 14:31:45 +0200

    meteor react

--- client/main.js
+++ client/main.js
@@ -1,2 +1,10 @@
+import React from 'react';
+import { Meteor } from 'meteor/meteor';
+import { render } from 'react-dom';
+
 import '../imports/startup/accounts-config.js';
-import '../imports/ui/body.js';
+import App from '../imports/ui/App.js';
+
+Meteor.startup(() => {
+  render(<App />, document.getElementById('render-target'));
+});
