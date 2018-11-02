7fb4a834a6afe8f195b18ffd8edd52bf20b5ae51
GodefroiRoussel <godefroi.roussel@gmail.com>
2018-11-02 15:05:48 +0100

    [WIP] Still getting Error Container CSS when compile successfully

--- client/server.js
+++ client/server.js
@@ -1,21 +1,44 @@
-var webpack = require('webpack');
-var WebpackDevServer = require('webpack-dev-server');
-var config = require('./webpack.config');
+require('dotenv').config();
 
-new WebpackDevServer(webpack(config), {
-  contentBase: './public',
-  publicPath: config.output.publicPath,
-  hot: true,
-  historyApiFallback: true,
-  colors: true,
-  noInfo: true,
-  watchOptions: {
-    aggregateTimeout: 300,
-    poll: 1000
-  },
-}).listen(3000, 'localhost', function(err, result) {
-  if (err) {
-    console.log(err);
-  }
-  console.log('Listening at localhost:3000');
-});
+if (process.env.NODE_ENV != 'production') {
+  // Create a development serveur with hot reloading
+  var webpack = require('webpack');
+  var WebpackDevServer = require('webpack-dev-server');
+  var config = require('./webpack.config');
+
+  new WebpackDevServer(webpack(config), {
+    contentBase: './public',
+    publicPath: config.output.publicPath,
+    hot: true,
+    historyApiFallback: true,
+    /*colors: true,
+    noInfo: true,
+    watchOptions: {
+      aggregateTimeout: 300,
+      poll: 1000
+    },*/
+  }).listen(3000, 'localhost', function (err, result) {
+    if (err) {
+      console.log(err);
+    }
+    console.log('Listening at http://localhost:3000');
+  });
+} else {
+  // Create express server to distribute the application
+  var path = require('path');
+  var express = require('express');
+
+
+  var app = express();
+
+  var staticPath = path.join(__dirname, './public');
+  app.use(express.static(staticPath));
+
+  // Allows you to set port in the project properties.
+  app.set('port', process.env.PORT || 1234);
+
+  app.listen(app.get('port'), function () {
+    console.log('listening on port ' + app.get('port'));
+
+  });
+}
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -5,17 +5,18 @@ var path = require('path');
 var webpack = require('webpack');
 var HtmlWebpackPlugin = require('html-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const devMode = process.env.NODE_ENV !== 'production'
+const postcssNormalize = require('postcss-normalize');
 
 
 module.exports = {
-  devtool: 'eval',
-  entry: [
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    './app/App.js'
-  ],
+  //mode: 'development',
+  entry: //'./app/App.js',
+    [
+      'webpack-dev-server/client?http://localhost:3000',
+      'webpack/hot/only-dev-server',
+      './app/App.js'
+    ],
   plugins: [
     //new CleanWebpackPlugin(['public']),
     new HtmlWebpackPlugin({
@@ -24,15 +25,15 @@ module.exports = {
       inject: 'body'
     }),
     new webpack.HotModuleReplacementPlugin(),
-    new MiniCssExtractPlugin({
+    /*new MiniCssExtractPlugin({
       // Options similar to the same options in webpackOptions.output
       // both options are optional
       filename: devMode ? '[name].css' : '[name].[hash].css',
       chunkFilename: devMode ? '[id].css' : '[id].[hash].css',
-    })
+    })*/
   ],
   output: {
-    pathinfo: true,
+    //pathinfo: true,
     path: path.resolve(__dirname, 'public'),
     filename: 'bundle.js',
     //publicPath: 'http://localhost:3000/'
@@ -63,23 +64,39 @@ module.exports = {
         }
       },
       {
-        test: /\.(sc|c)ss$/,
+        test: /\.css$/,
         use: [
-          devMode ? 'style-loader' : MiniCssExtractPlugin.loader,
-          { loader: "style-loader" },
-          { loader: "css-loader" },
-        ]
+          /*
+          'style-loader',
+          'css-loader?sourceMap&importLoaders=2&localIdentName=[name]__[local]__[hash:base64:5]!postcss'
+          */
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 1 } },
+          {
+            loader: 'postcss-loader'
+          }
+          //'css-loader?modules&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]'
+        ],
       },
       {
         test: /\.styl$/,
-        exclude: /node_modules/,
         use: [
+          /*
           'style-loader',
-          'css-loader',
+          'css-loader?modules&importLoaders=2&localIdentName=[name]__[local]__[hash:base64:5]!postcss',
+          */
+          'style-loader',
+          { loader: 'css-loader', options: { importLoaders: 2 } },
+          {
+            loader: 'postcss-loader'
+          },
           {
             loader: 'stylus-loader',
-          },
-        ],
+            options: {
+              use: [sGrid(), rupture()]
+            }
+          }
+        ]
       },
       {
         test: /\.(png|svg|jpg|gif)$/,
@@ -87,12 +104,23 @@ module.exports = {
         use: [
           'file-loader'
         ]
-      }
+      },/*
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [
+
+          //MiniCssExtractPlugin.loader,
+          'style-loader',
+          'css-loader'
+
+        ]
+      },*/
     ]
   },
   resolve: {
     extensions: ['.js', '.jsx', '.json', '.css', '.styl', '.png', '.jpg', '.jpeg', '.gif']
-  },
+  }
 };
 /*
 module.exports = {
