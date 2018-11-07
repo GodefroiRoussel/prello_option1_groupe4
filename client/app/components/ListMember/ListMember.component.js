e0b1c9bdee539c03e9477fc4dcb0d1b038e7b371
James TERRIEN <jamesterrien95@hotmail.fr>
2018-11-07 16:15:17 +0100

    end team front tabs

--- client/app/components/ListMember/ListMember.component.js
+++ client/app/components/ListMember/ListMember.component.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Tab, Card, Image, List, Button , Header, Input, Loader, Dropdown, Grid} from 'semantic-ui-react';
+import logoAnonymous from '../../styles/assets/hanonyme.png'
+import style from './listMember.styl';
+import defaultStyle from "../../styles/settings.styl";
 
 const ListMember = (props) => {
 
@@ -12,25 +15,25 @@ const ListMember = (props) => {
     return (<div>
         <div>
             <Input list='members' placeholder='Choose members to add...' />
+            <Button onClick={callAddMember}>ADD</Button>
         </div>
     <List animated verticalAlign='middle'>
-    <Grid columns={5}></Grid>
     {
         props.members.map((x, i) =>{
             return(
-                <Grid.Column>
+
                     <List.Item>
-                        <Image avatar src='https://react.semantic-ui.com/images/avatar/small/helen.jpg' />
+                        <Image avatar src={logoAnonymous} />
                         <List.Content>
-                        <List.Header>{x}</List.Header>
+                        <List.Header className={defaultStyle.textColor1}>{x}</List.Header>
+                            <span className={defaultStyle.textColor4}>{x==props.ownerTeam ? 'admin' : 'member'}</span>
                         </List.Content>
                     </List.Item>
-                </Grid.Column>
+
             )
         })
     }
     </List>
-    <Button onClick={callAddMember}>ADD</Button>
       </div>)
 }
 
--- client/app/containers/Team/Team.js
+++ client/app/containers/Team/Team.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import cssModules from 'react-css-modules';
-import { Tab, Card, Image, List, Button , Grid} from 'semantic-ui-react';
+import { Tab, Card, Image, List, Button ,Form, Grid, Segment,Select} from 'semantic-ui-react';
 import ListMember from '../../components/ListMember/ListMember.component';
 import CardBoards from '../../components/CardBoards/CardBoards.component';
 import { callAddMember, callRemoveTeam } from '../../objects/Team/TeamAsyncActions';
@@ -10,16 +10,40 @@ import { browserHistory } from 'react-router';
 import style from './team.styl';
 import defaultStyle from "../../styles/settings.styl";
 
+
 import classNames from 'classnames'
 
+const options = [
+    {key:'0', text: 'private', value: "false"},
+    {key:'1', text: 'public', value: "true"},
+]
 
 
 class Team extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            team: ""
+            team: "",
+            privacy: "false"
         };
+    }
+
+    changePrivacyValue=(e, {value})=>{
+        this.setState({
+            privacy: value
+        });
+    }
+
+    componentWillReceiveProps(nextProps) {
+        if(this.props != nextProps) {
+            if(nextProps.team.visibilityTeam)
+            {
+                this.setState({
+                    privacy: nextProps.team.visibilityTeam
+                });
+            }
+
+        }
     }
 
     render() {
@@ -30,10 +54,10 @@ class Team extends React.Component {
             this.state.team = this.props.team;
             return (
                 <Grid centered style={style.root}>
-                    <Grid.Column mobile={12} tablet={8} computer={6}>
+                    <Grid.Column mobile={16} tablet={8} computer={6}>
                         <h1 className={defaultStyle.textColor4}>{this.props.team.nameTeam}</h1>
                     </Grid.Column>
-                    <Grid.Column mobile={12} tablet={12} computer={12}>
+                    <Grid.Column mobile={15} tablet={12} computer={12}>
                         <div className={style.teamBox}>
                             <Tab className={style.tabTeam} panes={this.panes} />
                         </div>
@@ -45,20 +69,29 @@ class Team extends React.Component {
 
     panes = [
         { menuItem: {key: 'boards',  content: 'Boards'}, render: () => <Tab.Pane><CardBoards boards={this.props.team.idBoards}/></Tab.Pane> },
-        { menuItem: {key: 'users', content: 'Members'}, render: () => <Tab.Pane><ListMember id={this.props.team._id} members={this.props.team.members} addMembers={this.props.DispatchCallAddMember}/></Tab.Pane>},
+        { menuItem: {key: 'users', content: 'Members'}, render: () => <Tab.Pane><ListMember id={this.props.team._id} ownerTeam={this.props.ownerTeam} members={this.props.team.members} addMembers={this.props.DispatchCallAddMember}/></Tab.Pane>},
         { menuItem: {key: 'setting',  content: 'Settings'}, render: () => <Tab.Pane>{this.settings}</Tab.Pane> },
     ]
 
     settings = (
         <div>
-            <h3>Team visibility</h3>
-            <p><strong>Private</strong> This team is </p>
-            <span>
-                    visibility of the team is
-            </span>
-            <h3>Restrictions</h3>
-            <h3>Creating board Restrictions</h3>
-            <Button onClick={this.handleDeleteTeam.bind(this)}>delete</Button>
+            <h3 className={defaultStyle.textColor1}>Team visibility</h3>
+            <Form.Group inline>
+                <label className={defaultStyle.textColor4}>This team is </label>
+                <Select
+                    onChange={this.changePrivacyValue.bind(this)}
+                    options={options}
+                    name="privacy"
+                    className={style.privacySelect}
+                >
+                </Select>
+
+
+            </Form.Group>
+
+            <h3  className={defaultStyle.textColor1}>Actions</h3>
+
+            <Button onClick={this.handleDeleteTeam.bind(this)}>Delete the team</Button>
         </div>
     )
 
