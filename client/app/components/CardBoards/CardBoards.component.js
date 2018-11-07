2a395b4d5fd208b01e3cc948895cc591c0724bc8
James TERRIEN <jamesterrien95@hotmail.fr>
2018-11-07 14:37:35 +0100

    board Card list front

--- client/app/components/CardBoards/CardBoards.component.js
+++ client/app/components/CardBoards/CardBoards.component.js
@@ -1,28 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { Tab, Card, Image, List, Button , Header, Input, Loader, Dropdown, Modal} from 'semantic-ui-react';
+import { Tab, Card, Image, List, Button , Header, Input, Loader, Dropdown, Modal, Icon} from 'semantic-ui-react';
+import defaultStyle from "../../styles/settings.styl";
+import style from './cardBoards.styl';
+import cssModules from 'react-css-modules';
 
 const CardBoards = (props) => {
     return (
-    <div>
+    <div style={style.root}>
         <Card.Group>
             {props.boards.map(x => {
                     return (
-                    <Card key={x}>
+                    <Card key={x} className={style.cardBoard}>
                         <Card.Content>
-                        <Card.Header>{x}</Card.Header>
+                            <Card.Header className={style.cardBoardHeader}>{x}</Card.Header>
+                            <Card.Meta className={style.cardBoardMeta}>other infos</Card.Meta>
                         </Card.Content>
                     </Card>)
                 })
             }
-            <Card>
-                <Card.Content>
-                    <Card.Header>Add a board</Card.Header>
-                </Card.Content>
-            </Card>
+            <div className={style.buttonAddBoardTeam}>
+                <Button  icon>
+                    <Icon name='add' />
+                </Button>
+            </div>
+
+
         </Card.Group>
       </div>
       )
 }
 
-export default CardBoards;
+export default cssModules(CardBoards, style);
--- client/app/containers/Team/Team.js
+++ client/app/containers/Team/Team.js
@@ -8,6 +8,9 @@ import { callAddMember, callRemoveTeam } from '../../objects/Team/TeamAsyncActio
 import {callGetAllUser} from '../../objects/User/UserAsyncActions';
 import { browserHistory } from 'react-router';
 import style from './team.styl';
+import defaultStyle from "../../styles/settings.styl";
+
+import classNames from 'classnames'
 
 
 
@@ -26,25 +29,25 @@ class Team extends React.Component {
         else{
             this.state.team = this.props.team;
             return (
-                <div styleName="test">
-                    <Grid>
-                        <Grid.Column>
-                        <h1>{this.props.team.nameTeam}</h1>
-                            <div>
-                                <Tab panes={this.panes}/>
+                <Grid centered style={style.root}>
+                    <Grid.Column mobile={12} tablet={8} computer={6}>
+                        <h1 className={defaultStyle.textColor4}>{this.props.team.nameTeam}</h1>
+                    </Grid.Column>
+                    <Grid.Column mobile={12} tablet={12} computer={12}>
+                        <div className={style.teamBox}>
+                            <Tab className={style.tabTeam} panes={this.panes} />
                         </div>
-                        </Grid.Column>
-                    </Grid>
-                </div>
+                    </Grid.Column>
+                </Grid>
             )
         }
     }
 
     panes = [
-        { menuItem: {key: 'boards', icon: 'table', content: 'Tableaux'}, render: () => <Tab.Pane><CardBoards boards={this.props.team.idBoards}/></Tab.Pane> },
-        { menuItem: {key: 'users', icon: 'users', content: 'Membres'}, render: () => <Tab.Pane><ListMember id={this.props.team._id} members={this.props.team.members} addMembers={this.props.DispatchCallAddMember}/></Tab.Pane>},
-        { menuItem: {key: 'setting', icon: 'setting', content: 'Paramètres'}, render: () => <Tab.Pane>{this.settings}</Tab.Pane> },
-      ]
+        { menuItem: {key: 'boards',  content: 'Boards'}, render: () => <Tab.Pane><CardBoards boards={this.props.team.idBoards}/></Tab.Pane> },
+        { menuItem: {key: 'users', content: 'Members'}, render: () => <Tab.Pane><ListMember id={this.props.team._id} members={this.props.team.members} addMembers={this.props.DispatchCallAddMember}/></Tab.Pane>},
+        { menuItem: {key: 'setting',  content: 'Settings'}, render: () => <Tab.Pane>{this.settings}</Tab.Pane> },
+    ]
 
     settings = (
         <div>
@@ -66,16 +69,16 @@ class Team extends React.Component {
 
 };
 
-  function mapStateToProps(state, ownProps){
-      return{
+function mapStateToProps(state, ownProps){
+    return{
         team: state.teams.find(el => el._id == ownProps.location.state.team),
         teams: state.teams,
-      }
-  };
+    }
+};
 
-  const mapDispatchToProps = (dispatch)=> ({
-      DispatchCallAddMember: data => dispatch(callAddMember(data)),
-      DispatchCallRemoveTeam: data => dispatch(callRemoveTeam(data)),
-  });
-  
-  export default connect(mapStateToProps, mapDispatchToProps)(cssModules(Team, style));
+const mapDispatchToProps = (dispatch)=> ({
+    DispatchCallAddMember: data => dispatch(callAddMember(data)),
+    DispatchCallRemoveTeam: data => dispatch(callRemoveTeam(data)),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(cssModules(Team, style));
